c65dff66608ae53ab5698200662e5799ca28e5df
Arcelino Cavalcante <arcelinocavalcante@MacBook-Air-de-Arcelino.local>
2026-01-29 20:58:37 -0300

    Fix: Sync lockfile and update baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "arcelino-cavalcante.github.io/meu-blog-obsidian",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
