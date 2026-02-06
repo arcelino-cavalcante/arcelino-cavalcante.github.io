be305581b39035610d67ffa55636c236b153d1fa
Arcelino Cavalcante <arc@MacBook-Air-de-Arcelino.local>
2026-02-06 13:56:13 -0300

    Quartz sync: Feb 6, 2026, 1:56 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -87,7 +87,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [],
 }
