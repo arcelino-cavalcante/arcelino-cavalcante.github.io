72d7361502303aa89cc29927470e6785ada093a7
Arcelino Cavalcante <arc@MacBook-Air-de-Arcelino.local>
2026-02-06 20:48:10 -0300

    Quartz sync: Feb 6, 2026, 8:48 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,12 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer({ title: "Categorias" }),
+    Component.Explorer({
+      title: "Categorias",
+      mapFn: (node) => {
+        node.displayName = node.displayName.replace(/^\d+[-_.\s]+/, "")
+      },
+    }),
   ],
   right: [
     Component.Graph({
@@ -87,7 +92,12 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer({ title: "Categorias" }),
+    Component.Explorer({
+      title: "Categorias",
+      mapFn: (node) => {
+        node.displayName = node.displayName.replace(/^\d+[-_.\s]+/, "")
+      },
+    }),
   ],
   right: [],
 }
