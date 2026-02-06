4e3c10fdaf32f44fd3c4e940e5272c18ea48c0a1
Arcelino Cavalcante <arc@MacBook-Air-de-Arcelino.local>
2026-02-06 13:13:23 -0300

    Quartz sync: Feb 6, 2026, 1:13 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,8 +28,16 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
@@ -69,8 +77,16 @@ export const defaultListPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
