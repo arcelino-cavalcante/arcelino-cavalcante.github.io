cdc2d1bc6d46da8de11098a37b1a9df699758257
Arcelino Cavalcante <arc@MacBook-Air-de-Arcelino.local>
2026-02-06 21:58:24 -0300

    Quartz sync: Feb 6, 2026, 9:58 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -91,6 +91,7 @@ const config: QuartzConfig = {
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
       Plugin.Offline(),
+      Plugin.CNAME(),
     ],
   },
 }
